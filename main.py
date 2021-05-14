d78c2c5e6be6198716e8226066d1de3e03b80b78
RakunOFC <84198166+RakunOFC@users.noreply.github.com>
2021-05-14 14:31:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ else:
 '''
 token = ["f01e0024a26baef3cc53a2ac208dd141","5072097263","25d800a8b8e8b99d77c809567aa291b8"]
 
-welcome_msg = ["Que a força esteja com você", "Bem vindo", "Você é um mito", "Okaerinasai", "Esta pessoa é muito boa no Websexo ->", "Você é um baitola", "Você é corno", "Você é gay"]
+welcome_msg = ["Que a força esteja com você", "Bem vindo", "Você é um mito", "Okaerinasai", "Esta pessoa é muito boa no Websexo ->", "Você é um baitola", "Você é corno", "Você é foda"]
 try:
     if __name__ == '__main__':
         os.system("clear")
